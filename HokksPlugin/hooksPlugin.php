a6b8f97e64614a74d3e940fe6a603c743ee03eeb
ERFANim <faghanimohamaderfan@gmail.com>
2023-08-27 17:06:53 +0330

    init-plugin-wp-learn

---
+++ HokksPlugin/hooksPlugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Hooks Plugin
+Plugin URI: https://mywebsite.com/
+Description: this is hooks plugin
+Author: My Name
+Author URI: https://Mypersonalwensite.com/
+Text Domain: myfirstplugin
+Domain Path: /languages/
+Version: 1.0.0
+*/
---
+++ MyFirstPlugin.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: My First Plugin
+Plugin URI: https://mywebsite.com/
+Description: my plugin description
+Author: My Name
+Author URI: https://Mypersonalwensite.com/
+Text Domain: myfirstplugin
+Domain Path: /languages/
+Version: 1.0.0
+*/
+
+/*function showMyNameInWordpress($name)
+{
+    echo "My Name Is {$name}";
+}
+
+showMyNameInWordpress("ERFANim");*/
+
+function doSomeAction(){
+
+    runAfterSomeAction1();
+    runAfterSomeAction2();
+    runAfterSomeAction3();
+}
+
+function runAfterSomeAction1(){}
+
+function runAfterSomeAction2(){}
+
+function runAfterSomeAction3(){}
