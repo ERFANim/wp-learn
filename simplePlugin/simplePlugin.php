f4218f5a4ab387fb71efc23d092fb351dfc6a430
ERFANim <faghanimohamaderfan@gmail.com>
2023-08-27 21:40:55 +0330

    activation hook

--- simplePlugin/simplePlugin.php
+++ simplePlugin/simplePlugin.php
@@ -10,6 +10,24 @@ Domain Path: /languages/
 Version: 1.0.0
 */
 
+
+//path and url define
 define('PLUGIN_DIR',plugin_dir_path(__FILE__));
 
 define('PLUGIN_URL',plugin_dir_url(__FILE__));
+
+
+//active hook
+function simple_plugin_activation(){
+
+
+}
+
+function simple_plugin_deactivation(){
+
+
+}
+
+register_activation_hook(__FILE__,'simple_plugin_activation');
+
+register_deactivation_hook(__FILE__,'simple_plugin_deactivation');
