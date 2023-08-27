0476616cba8b82930a88316960c7faa35bdf2f6b
ERFANim <faghanimohamaderfan@gmail.com>
2023-08-27 19:05:58 +0330

    init-simple-plugin

---
+++ simplePlugin/simplePlugin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Simple Plugin 
+Plugin URI: https://mywebsite.com/
+Description: a simple plugin
+Author: ERFAN
+Author URI: https://Mypersonalwensite.com/
+Text Domain: myfirstplugin
+Domain Path: /languages/
+Version: 1.0.0
+*/
+
