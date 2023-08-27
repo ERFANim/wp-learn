8add922a119c50fa21bbab9728ce52e9695b898c
ERFANim <faghanimohamaderfan@gmail.com>
2023-08-27 17:39:08 +0330

    action-hook-wp-learn

--- HokksPlugin/hooksPlugin.php
+++ HokksPlugin/hooksPlugin.php
@@ -9,3 +9,20 @@ Text Domain: myfirstplugin
 Domain Path: /languages/
 Version: 1.0.0
 */
+
+function print_order_id(){
+    print "Order_Purchased!";
+}
+
+function send_order_email(){
+    print "Order Email Sent!";
+}
+
+add_action('order_Purchased','send_order_email');
+add_action('order_Purchased','print_order_id');
+
+
+
+do_action('order_Purchased');
+
+
