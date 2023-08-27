5e17b15ec01768c1897632634d1082e98c458ea4
ERFANim <faghanimohamaderfan@gmail.com>
2023-08-27 18:19:16 +0330

    filter-hook-wp-learn

--- HokksPlugin/hooksPlugin.php
+++ HokksPlugin/hooksPlugin.php
@@ -10,6 +10,8 @@ Domain Path: /languages/
 Version: 1.0.0
 */
 
+
+/*actions hooks*/
 function print_order_id(){
     print "Order_Purchased!";
 }
@@ -21,8 +23,22 @@ function send_order_email(){
 add_action('order_Purchased','send_order_email');
 add_action('order_Purchased','print_order_id');
 
-
-
 do_action('order_Purchased');
 
 
+/*filters hooks*/
+
+function update_order_price1($order_price){
+    return $order_price * 2 ;
+}
+
+function update_order_price2($order_price){
+    return $order_price / 5 ;
+}
+
+add_filter('get_order_price','update_order_price1');
+add_filter('get_order_price','update_order_price2');
+
+$result = apply_filters('get_order_price',25000);
+
+print "$result";
